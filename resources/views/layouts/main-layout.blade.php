b26959356891c8836c659578aa34e2d000c75297
luigicara <luigi.caravelli98@gmail.com>
2023-02-09 20:52:42 +0100

    added main-layout

---
+++ resources/views/layouts/main-layout.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <!-- Usando Vite -->
+    @vite(['resources/js/app.js'])
+</head>
+<body>
+    @include('components.header')
+    @include('components.errors')
+    @yield('content')
+    {{-- @include('components.footer') --}}
+</body>
+</html>
